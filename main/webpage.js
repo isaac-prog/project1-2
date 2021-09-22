6d1c8efb3046c1bd3ae1f25e2cea409d7f81daec
isaac-prog <panda_mm88@yahoo.com>
2021-09-22 06:33:45 +0000

    attempted 1 page display

--- main/webpage.js
+++ main/webpage.js
@@ -6,3 +6,23 @@ function hamburgerHTML() {
       x.className = x.className.replace(" display-burger", "");
     }
   }
+// select all the page buttons
+let allButtons = document.querySelectorAll('.page-button');
+
+for (let button of allButtons) {
+    button.addEventListener('click', function(event){
+        // find out which button hs been pressed
+        let clickedButton = event.target;
+        let pageNumber = clickedButton.dataset.page;
+
+        let allPages = document.querySelectorAll('.page');
+            for (let p of allPages) {
+                p.classList.remove('show');
+                p.classList.add('hidden');
+            }
+        
+        // only show page that the button is for
+        document.querySelector("#page-" + pageNumber).classList.remove('hidden');
+        document.querySelector("#page-" + pageNumber).classList.add('show');
+    })
+}
